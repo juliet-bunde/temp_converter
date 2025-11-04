e1fa106cb61907b10e3b49e3900902cacd822f33
Juliet Bunde <jbunde@OAKAR.local>
2025-11-04 15:46:52 +0300

    update temperature converter input handling and styling

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 function convertTemperature(){
-    const inputTemp = parseFloat(document.getElementById('inputTemp').value);
+    const inputTemp = parseFloat(document.getElementById('tempInput').value);
     const selectUnit = document.getElementById('unitSelect').value;
+    const resultElement = document.getElementById('result');
     let result = "";
 
     if(isNaN(inputTemp)){
         result = "Please enter a valid number.";
-   
-    }
+        resultElement.innerText = result;
+        return;
+    } 
+
+    
+    let Celsius,Fahrenheit, Kelvin;
+
+    /*convert input to all selected units based on user selection*/
+
+    if(selectUnit === "Celsius") {
+     Celsius = inputTemp;
+    Fahrenheit = (Celsius * 9/5)+32;
+    Kelvin = Celsius + 273.15;
+ }else if(selectUnit === "Fahrenheit"){
+    Fahrenheit = inputTemp;
+    Celsius = (Fahrenheit - 32)*5/9;
+    Kelvin = Celsius + 273.15;
+ }else if(selectUnit ==="Kelvin"){
+    Kelvin = inputTemp;
+    Celsius = Kelvin - 273.15;
+    Fahrenheit = (Celsius * 9/5)+32;
+ }
+ //display results
+ result = `${Celsius.toFixed(2)} °C = ${Fahrenheit.toFixed(2)}°F = ${Kelvin.toFixed(2)}°K`;
+  resultElement.innerText = result;
+}
+
+
